4cec8fbe1762ecf4b85d87b3060d842d72119859
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-02-20 17:26:25 +0900

    자료구조/알고리즘/시작

---
+++ basic/algorithm01/app01/app01.c
@@ -0,0 +1,33 @@
+﻿// p13 알고리즘이란?
+// 세 정수를 입력, 최댓값구하기
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+	int a, b, c;
+	int max = 0;
+
+
+	printf(" 세정수 중 최댓값을 리턴.\n");
+
+	printf("a > "); scanf("%d", &a);
+	printf("b > "); scanf("%d", &b);
+	printf("c > "); scanf("%d", &c);
+
+	// 1, 5, 3
+
+	max = a;
+	if (b > max) {
+		max = b;
+	}
+	if (c > max) {
+		max = c;
+	}
+
+	printf("최댓값은 %d\n", max);
+
+	return 0;
+
+}
