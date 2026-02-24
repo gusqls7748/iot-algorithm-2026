4835750aebb5e2fe2f6efe6480a5e26bcec07827
chen hyenbeen <123910896+gusqls7748@users.noreply.github.com>
2026-02-24 17:29:10 +0900

    커밋

---
+++ basic/algorithm03/app02/app02.c
@@ -0,0 +1,40 @@
+﻿// app02 - 팩토리얼 재귀방식
+// result 를 int로 하면 12!까지만 계산가능
+// unsignd long long이면 20!까지 계산가능
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+unsigned long long factorial(int n);
+
+int main(void) {
+	int x;
+	unsigned long long result = 0;
+	puts("팩토리얼 재귀호출");
+
+
+	while (1){
+	printf("정수 입력 > "); scanf("%d", &x);
+	if (x <= 0) break;
+
+	result = factorial(x);
+
+	printf("%d! = %llu \n", x, factorial(x));
+	}
+	return 0;
+}
+
+
+// factorial
+// 0!, 1! = 1
+// n! = n x (n -1)!
+unsigned long long factorial(int n) {
+	if (n > 0) { // n x (n - 1)!
+		// return n * factorial(n -1); // -1 // int 일 경우
+		return (unsigned long long)n * factorial(n - 1); // 5 * factorial(4) -> 5 * 4 * factorial(3)...
+	}
+	else { // 0!
+		return 1;	// 반드시 종료조건이 있어야 함!
+	}
+}
